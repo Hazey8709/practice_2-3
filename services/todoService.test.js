aebdacac751299231d726dbca43bf0ee011e1da0
Hazey8709 <jfortin802@gmail.com>
2023-03-08 18:17:24 -0500

    ud

---
+++ services/todoService.test.js
@@ -0,0 +1,21 @@
+const { todoService, todoServiceId } = require("../services/pokemonServ");
+
+describe("", () => {
+    test("", () => {
+        expect(uppercase("")).toBe("");
+    });
+
+    test("", () => {
+        expect(lowercase("")).toBe("");
+    });
+});
+
+
+
+
+// describe("Testing Basic Math", () => {
+//     test("Addition 2 num", () => {
+//         const results = operations.add(10, 10);
+//         expect(results).toBe(20);
+//     });
+// });
